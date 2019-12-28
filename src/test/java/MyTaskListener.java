8b34380edd98f6b30ba9a4f0bfb8a6016a239207
马治武 <zhiwu.ma@bespinglobal.cn>
2019-12-28 19:41:40 +0800

    add custom activity

---
+++ src/test/java/MyTaskListener.java
@@ -0,0 +1,9 @@
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+public class MyTaskListener implements TaskListener {
+    @Override
+    public void notify(DelegateTask delegateTask) {
+        delegateTask.setAssignee("fuck");
+    }
+}
--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -21,7 +21,6 @@ import java.util.Map;
 @SpringBootTest(classes = {Application.class})// 指定启动类
 public class TestDemo {
 
-
     /**
      * 1. 根据流程图 部署
      */
@@ -39,7 +38,7 @@ public class TestDemo {
 
         // 输出部署的一些信息
         System.out.println(deployment.getName()); // 请假申请单流程
-        System.out.println(deployment.getId()); // 3e014bc6-2956-11ea-ad22-feaf48b96e42
+        System.out.println(deployment.getId()); // a14b9e1b-2963-11ea-a6e9-86dbe108bd4e
     }
 
 
@@ -54,11 +53,12 @@ public class TestDemo {
         variables.put("startUserKey", "startUserKey");
         // 创建流程实例(关键步骤)即 启动流程实例
         // 找key的方法  1：bpmn文件中的id，它对应的值就是key 2：直接看数据库中流程定义表act_re_procdet的key值）
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process_id", variables);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process_id", "businessKey", variables);
+        // ProcessInstance processInstance = runtimeService.startProcessInstanceById("aec4c179-2964-11ea-98b5-86dbe108bd4e", "businessKey", variables);
         // 输出实例的相关信息
         System.out.println("流程部署ID=" + processInstance.getDeploymentId());//null
         System.out.println("流程定义ID=" + processInstance.getProcessDefinitionId());//process_id:2:6396446b-2956-11ea-8ff9-feaf48b96e42
-        System.out.println("流程实例ID=" + processInstance.getId());//d30d9429-2956-11ea-af98-feaf48b96e42
+        System.out.println("流程实例ID=" + processInstance.getId());// 44e22b4f-2965-11ea-a33d-86dbe108bd4e
         System.out.println("流程活动ID=" + processInstance.getActivityId());//获取当前具体执行的某一个节点的ID(null)
     }
 
@@ -69,22 +69,35 @@ public class TestDemo {
      * 做查询
      */
     @Test
-    public void query() {
+    public void submitLeaveRequest() {
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         TaskService taskService = processEngine.getTaskService();
 
         // 根据实例 id 查询
-        for (Task task : taskService.createTaskQuery().processInstanceId("d30d9429-2956-11ea-af98-feaf48b96e42").list()) {
+        for (Task task : taskService.createTaskQuery()
+                .processInstanceId("44e22b4f-2965-11ea-a33d-86dbe108bd4e").list()) {
             System.out.println(task.getId()); // d31164bd-2956-11ea-af98-feaf48b96e42
 
-            // 转让
-            taskService.setAssignee(task.getId(), "taskUserKey");
+            // 转让, 这里正式开始 提交请假单
+            // taskService.setAssignee(task.getId(), "taskUserKey");
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("name", "voidm");
+            map.put("age", 19);
+            System.out.println(map);
+            taskService.complete(task.getId(), map);
         }
+    }
+
+    @Test
+    public void approval() {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        TaskService taskService = processEngine.getTaskService();
         // 根据 processDefinitionKey 查询
         // 根据流程定义的key以及负责人 assignee 来实现当前用户的任务列表查询
         List<Task> taskList = taskService.createTaskQuery()
                 .processDefinitionKey("process_id")
-                .taskAssignee("taskUserKey")
+                .taskAssignee("fuck")
+                .includeProcessVariables()
                 .list();//这里还有一个查询唯一结果的方法：singleResult();、还有分页查询listPage(index,limit);
         // 任务列表展示
         for (Task task : taskList) {
@@ -96,20 +109,17 @@ public class TestDemo {
             System.out.println("任务负责人名称=" + task.getAssignee());
             //查的act_hi_taskinst表的NAME_
             System.out.println("任务名称=" + task.getName());
+            System.out.println(task.getProcessVariables());
+
+            taskService.complete(task.getId(), task.getProcessVariables());
+            System.out.println("人物完成：已经提交");
+
+            taskList = taskService.createTaskQuery()
+                    .processDefinitionKey("process_id")
+                    .taskAssignee("fuck")
+                    .includeProcessVariables()
+                    .list();
+            System.out.println("taskList.size() = " + taskList.size());
         }
-    }
-
-
-    /**
-     * 根据 taskId 完成人物
-     */
-    @Test
-    public void complete() {
-
-        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
-        TaskService taskService = processEngine.getTaskService();
-
-        // 处理任务,结合当前用户任务列表的查询操作的话，可以知道任务ID=5002(实际操作中应该与查询写在一起)
-        taskService.complete("d31164bd-2956-11ea-af98-feaf48b96e42");
     }
 }
